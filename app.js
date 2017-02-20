39254e9b47b5040f6951b97b3df695dde5da886a
xue <xueyiqiang_909@126.com>
2017-02-20 17:26:14 +0800

    添加model

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 var mongoose = require('./config/mongoose')
-
+var User = require('./models/user.server.model')
 //mongoose 导出的是一个生成数据库实例的方法
 var db = mongoose()
--- config/mongoose.js
+++ config/mongoose.js
@@ -5,5 +5,23 @@ module.exports = function(){
     var db = mongoose.connect(config.mongodb)
 
     require('../models/user.server.model.js')
+
+      mongoose
+        .connection
+        .on('connected', function () {
+            console.log('Mongoose connected to ' + config.mongodb)
+        })
+    mongoose
+        .connection
+        .on('error', function () {
+            console.log('Mongoose connection error')
+        })
+    mongoose
+        .connection
+        .on('disconnected', function () {
+            console.log('Mongoose disconnected')
+        })
+
+
     return db
 }
